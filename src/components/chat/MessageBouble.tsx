7ff366f51f5a37c0bf500c530277a0fcf9f9dff0
vasilecampeanu <vasilecampeanu.link@yahoo.com>
2023-04-10 09:32:12 +0300

    feat: Improved copy button.

--- src/components/chat/MessageBouble.tsx
+++ src/components/chat/MessageBouble.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { ThreeDots } from 'react-loader-spinner';
 import { MarkdownRenderer } from 'obsidian';
-import TurndownService from 'turndown';
 
 export interface MessageBubbleProps {
 	role: string;
@@ -17,7 +16,8 @@ export const MessageBubble: React.FC<MessageBubbleProps> = ({
 	isLoading,
 }) => {
 	const messageContentRef = useRef<HTMLDivElement>(null);
-
+	const [showConfirmation, setShowConfirmation] = useState(false);
+	
 	useEffect(() => {
 		if (messageContentRef.current) {
 			const context = {
@@ -38,79 +38,13 @@ export const MessageBubble: React.FC<MessageBubbleProps> = ({
 	}, [content]);
 
 	const copyTextWithMarkdown = async () => {
-		const html = messageContentRef.current?.innerHTML || '';
-		const turndownService = new TurndownService();
+		await navigator.clipboard.writeText(content);
 
-		turndownService.addRule('tables', {
-			filter: 'table',
-			replacement: function (content, node) {
-				const thead = node.querySelector('thead');
-				const tbody = node.querySelector('tbody');
-				const headerRows = thead ? Array.from(thead.rows) : [];
-				const bodyRows = tbody ? Array.from(tbody.rows) : [];
+		setShowConfirmation(true);
 
-				const table = headerRows.concat(bodyRows);
-
-				const tableMarkdown = table
-					.map((row, rowIndex) => {
-						const rowContent = (
-							'|' +
-							Array.from(row.cells)
-								.map((cell) => (cell.textContent ? cell.textContent.trim() : ''))
-								.join('|') +
-							'|'
-						);
-
-						if (rowIndex === 0 && headerRows.length > 0) {
-							const separatorRow = '|' + Array.from(row.cells)
-								.map((cell) => '---')
-								.join('|') + '|';
-
-							return rowContent + '\n' + separatorRow;
-						}
-
-						return rowContent;
-					}).join('\n');
-
-				return '\n\n' + tableMarkdown + '\n\n';
-			},
-		});
-
-		turndownService.addRule('codeblocks', {
-			filter: ['pre', 'code'],
-			replacement: function (content, node) {
-				if (node.nodeName === 'PRE' && node.firstElementChild?.nodeName === 'CODE') {
-					const codeContent = node.firstElementChild.textContent || '';
-					return '\n\n```\n' + codeContent + '\n```\n\n';
-				} else if (node.nodeName === 'CODE') {
-					return '`' + content + '`';
-				} else {
-					return content;
-				}
-			},
-		});
-
-		turndownService.addRule('mathblocks', {
-			filter: (node) => node.nodeName === 'MJX-CONTAINER',
-			replacement: function (content, node) {
-			  const originalMathMLNode = node.querySelector('mjx-assistive-mml');
-		  
-			  if (!originalMathMLNode) {
-				return '';
-			  }
-		  
-			  const originalTeX = originalMathMLNode.getAttribute('data-mjx-texinput');
-			  const displayMode = node.nodeType === Node.ELEMENT_NODE && (node as Element).getAttribute('display') === 'true';
-		  
-			  // Use double dollar signs for display mode and single dollar signs for inline mode
-			  const delimiter = displayMode ? '$$' : '$';
-		  
-			  return delimiter + (originalTeX || '') + delimiter;
-			},
-		  });
-
-		const markdown = turndownService.turndown(html);
-		await navigator.clipboard.writeText(markdown);
+		setTimeout(() => {
+			setShowConfirmation(false);
+		}, 1000);
 	};
 
 	return (
@@ -130,10 +64,16 @@ export const MessageBubble: React.FC<MessageBubbleProps> = ({
 				</div>
 			</div>
 			<div className="ow-bubble-ow-actions">
-				<div className="ow-actions">
-					<button className="ow-copy-button" onClick={copyTextWithMarkdown}>
-						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect width="13" height="13" x="9" y="9" rx="2" ry="2"></rect><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path></svg>
-					</button>
+				<div className="ow-bubble-ow-actions">
+					<div className="ow-actions">
+						<button className="ow-copy-button" onClick={copyTextWithMarkdown}>
+							{showConfirmation ? (
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-check"><polyline points="20 6 9 17 4 12"></polyline></svg>
+							) : (
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect width="13" height="13" x="9" y="9" rx="2" ry="2"></rect><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path></svg>
+							)}
+						</button>
+					</div>
 				</div>
 			</div>
 		</div>
