750a55ea4e883b487906337e4c362c5f9c43ed6c
vasilecampeanu <vasilecampeanu.link@yahoo.com>
2023-09-21 22:37:16 +0300

    feat: Seted up multiple reducers.

--- src/components/chat/WeaverContext.tsx
+++ src/components/chat/WeaverContext.tsx
@@ -1,24 +1,51 @@
-import Weaver from 'main';
-import React, { createContext, useReducer, ReactNode, useContext, FC, Dispatch } from 'react';
-import { Conversation } from 'interfaces/Conversation';
-import { ConversationManager } from 'utils/ConversationManager';
-import { ActionTypes } from 'types/ActionTypes';
+import React, {
+    createContext,
+    useReducer,
+    ReactNode,
+    useContext,
+    FC,
+    Dispatch,
+} from "react";
+import { Conversation } from "interfaces/Conversation";
+import { ConversationManager } from "utils/ConversationManager";
+import { ActionTypes } from "types/ActionTypes";
+import Weaver from "main";
 
-interface State {
+// Define state for chat
+interface ChatState {
     conversation: Conversation | null;
+}
+
+const initialChatState: ChatState = {
+    conversation: null,
 };
 
-const initialState: State = {
-    conversation: null
+// Define state for thread (empty for now)
+interface ThreadState {}
+
+const initialThreadState: ThreadState = {};
+
+// Define main state that encompasses chat and thread states
+interface WeaverState {
+    chat: ChatState;
+    thread: ThreadState;
+}
+
+const initialState: WeaverState = {
+    chat: initialChatState,
+    thread: initialThreadState,
 };
 
-interface Action { type: ActionTypes.CREATE_CONVERSATION; payload: Conversation }
+// Chat actions
+interface ChatAction {
+    type: ActionTypes.CREATE_CONVERSATION;
+    payload: Conversation;
+}
 
-type WeaverContextType = [State, Dispatch<Action>, ConversationManager];
+type Actions = ChatAction; // Add other actions as required
 
-const WeaverContext = createContext<WeaverContextType | undefined>(undefined);	
-
-const chatReducer = (state: State, action: Action): State => {
+// Chat reducer
+const chatReducer = (state: ChatState, action: Actions): ChatState => {
     switch (action.type) {
         case ActionTypes.CREATE_CONVERSATION:
             return { ...state, conversation: action.payload };
@@ -27,8 +54,33 @@ const chatReducer = (state: State, action: Action): State => {
     }
 };
 
-export const WeaverProvider: FC<{ children: ReactNode; plugin: Weaver }> = ({ children, plugin }) => {
-    const [state, dispatch] = useReducer(chatReducer, initialState);
+// Thread reducer (empty for now)
+const threadReducer = (state: ThreadState, action: Actions): ThreadState => {
+    return state; // Do nothing for now, add logic as needed
+};
+
+// Main weaver reducer combining chat and thread reducers
+const weaverReducer = (
+    state: WeaverState,
+    action: Actions
+): WeaverState => {
+    return {
+        chat: chatReducer(state.chat, action),
+        thread: threadReducer(state.thread, action),
+    };
+};
+
+type WeaverContextType = [WeaverState, Dispatch<Actions>, ConversationManager];
+
+const WeaverContext = createContext<WeaverContextType | undefined>(
+    undefined
+);
+
+export const WeaverProvider: FC<{ children: ReactNode; plugin: Weaver }> = ({
+    children,
+    plugin,
+}) => {
+    const [state, dispatch] = useReducer(weaverReducer, initialState);
     const manager = new ConversationManager(plugin);
 
     return (
@@ -42,7 +94,7 @@ export const useWeaver = (): WeaverContextType => {
     const context = useContext(WeaverContext);
 
 	if (!context) {
-        throw new Error('useChat must be used within a WeaverProvider');
+        throw new Error("useWeaver must be used within a WeaverProvider");
     }
 
     return context;
--- src/hooks/useChatOperations.ts
+++ src/hooks/useChatOperations.ts
@@ -10,7 +10,7 @@ export const useChatOperations = () => {
     };
 
     return {
-        ...state,
+        ...state.chat,
         createNewConversation,
     };
 };
